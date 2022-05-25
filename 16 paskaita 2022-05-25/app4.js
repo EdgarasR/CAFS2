495593e0ee108ab17cd7dc904a1ce89c014cc971
Edgaras <edgaras.rcns@gmail.com>
2022-05-25 21:11:20 +0300

    JS tasks

---
+++ 16 paskaita 2022-05-25/app4.js
@@ -0,0 +1,6 @@
+let list = "\tI am a \"double quoted\" string inside \"double quotes\"\n" +
+            "FirstLine\n" +
+            "\\SecondLine\n" +
+            "ThirdLine";
+
+console.log(list);
