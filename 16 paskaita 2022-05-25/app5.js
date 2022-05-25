2a0615d89e9b195da9255b2c97719e8f0acbc81b
Edgaras <edgaras.rcns@gmail.com>
2022-05-25 21:50:28 +0300

    JS tasks

---
+++ 16 paskaita 2022-05-25/app5.js
@@ -0,0 +1,27 @@
+// Assignment Operators
+let str = 'Hello World!';
+let otherStr = str;
+console.log(str, otherStr);
+
+// Arithmetic Operators
+console.log(1 + 1);
+console.log(4 * 5);
+console.log(12 / 3);
+console.log(10 - 8);
+console.log(12 / 5, 12 % 5);
+
+let num = 4;
+num++;
+num++;
+console.log(num);
+
+num--
+console.log(num);
+
+num = 1;
+console.log(num++);
+console.log(++num);
+
+num = 1;
+num += 5;
+console.log(num);
