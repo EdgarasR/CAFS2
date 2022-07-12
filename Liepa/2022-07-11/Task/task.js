40f748d02bfa61f00d6f437ffcca8f57c84cd094
Edgaras <edgaras.rcns@gmail.com>
2022-07-12 18:44:05 +0300

    Update

--- Liepa/2022-07-11/Task/task.js
+++ Liepa/2022-07-11/Task/task.js
@@ -1,30 +1,38 @@
 function getPost() {
-    return new Promise(function(resolve,reject) {
-        let response = fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
-            response.json();
-        });
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        type: 'GET',
+        data: {
+          key: 'value',
+        },
+        success: function (data) {
+          resolve(data)
+        },
+        error: function (error) {
+          reject(error)
+        },
+      });
     });
 };
+document.querySelector('button')?.addEventListener('click', async function() {
 
-async function showData() {
-    let posts = await getPost();
-    let table = document.querySelector('#data');
-
-    for (let post of posts) {
-        let createTr = document.createElement('tr');
-        let createTdID = document.createElement('td');
-        let createTdUserID = document.createElement('td');
-        let createTdTitle = document.createElement('td');
-
-        createTdID.textContent = post.id;
-        createTdUserID.textContent = post.userId;
-        createTdTitle.textContent = post.title;
-
-        createTr.appendChild(createTdID);
-        createTr.appendChild(createTdUserID);
-        createTr.appendChild(createTdTitle);
-        table.appendChild(createTr);
-    };
-};
-
-showData();
+        let posts = await getPost();
+        let table = document.querySelector('#data');
+    
+        for (let post of posts) {
+            let createTr = document.createElement('tr');
+            let createTdID = document.createElement('td');
+            let createTdUserID = document.createElement('td');
+            let createTdTitle = document.createElement('td');
+    
+            createTdID.textContent = post.id;
+            createTdUserID.textContent = post.userId;
+            createTdTitle.textContent = post.title;
+    
+            createTr.appendChild(createTdID);
+            createTr.appendChild(createTdUserID);
+            createTr.appendChild(createTdTitle);
+            table.appendChild(createTr);
+        };
+});
