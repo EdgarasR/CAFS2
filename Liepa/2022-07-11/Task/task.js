d3300a7cf53dd42c6f3ed299fddb8bc450093732
Edgaras <edgaras.rcns@gmail.com>
2022-07-12 20:41:08 +0300

    Task2

--- Liepa/2022-07-11/Task/task.js
+++ Liepa/2022-07-11/Task/task.js
@@ -1,38 +1,40 @@
 function getPost() {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        type: 'GET',
-        data: {
-          key: 'value',
-        },
-        success: function (data) {
-          resolve(data)
-        },
-        error: function (error) {
-          reject(error)
-        },
-      });
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/users",
+      type: "GET",
+      data: {
+        key: "value",
+      },
+      success: function (data) {
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
     });
-};
-document.querySelector('button')?.addEventListener('click', async function() {
+  });
+}
+document.querySelector("button")?.addEventListener("click", async function () {
+  let posts = await getPost();
+  let table = document.querySelector("#data");
 
-        let posts = await getPost();
-        let table = document.querySelector('#data');
-    
-        for (let post of posts) {
-            let createTr = document.createElement('tr');
-            let createID = document.createElement('td');
-            let createUserID = document.createElement('td');
-            let createTitle = document.createElement('td');
-    
-            createID.textContent = post.id;
-            createUserID.textContent = post.userId;
-            createTitle.textContent = post.title;
-    
-            createTr.appendChild(createID);
-            createTr.appendChild(createUserID);
-            createTr.appendChild(createTitle);
-            table.appendChild(createTr);
-        };
+  for (let post of posts) {
+    let createTr = document.createElement("tr");
+    let createName = document.createElement("td");
+    let createID = document.createElement("td");
+    let createUsername = document.createElement("td");
+    let createEmail = document.createElement("td");
+
+    createID.textContent = post.id;
+    createUsername.textContent = post.username;
+    createName.textContent = post.name;
+    createEmail.textContent = post.email;
+
+    createTr.appendChild(createID);
+    createTr.appendChild(createName);
+    createTr.appendChild(createUsername);
+    createTr.appendChild(createEmail);
+    table.appendChild(createTr);
+  }
 });
