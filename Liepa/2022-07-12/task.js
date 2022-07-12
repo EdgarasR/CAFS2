5687b9e5a8fe0a5577096401d60dfaf90e569999
Edgaras <edgaras.rcns@gmail.com>
2022-07-12 21:22:39 +0300

    updt

--- Liepa/2022-07-12/task.js
+++ Liepa/2022-07-12/task.js
@@ -1,6 +1,23 @@
 function getPost() {
   return new Promise((resolve, reject) => {
     $.ajax({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      type: "GET",
+      data: {
+        key: "value",
+      },
+      success: function (data) {
+        resolve(data);
+      },
+      error: function (error) {
+        reject(error);
+      },
+    });
+  });
+}
+function getNames() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
       url: "https://jsonplaceholder.typicode.com/users",
       type: "GET",
       data: {
@@ -15,26 +32,7 @@ function getPost() {
     });
   });
 }
+
 document.querySelector("button")?.addEventListener("click", async function () {
-  let posts = await getPost();
-  let table = document.querySelector("#data");
-
-  for (let post of posts) {
-    let createTr = document.createElement("tr");
-    let createName = document.createElement("td");
-    let createID = document.createElement("td");
-    let createUsername = document.createElement("td");
-    let createEmail = document.createElement("td");
-
-    createID.textContent = post.id;
-    createUsername.textContent = post.username;
-    createName.textContent = post.name;
-    createEmail.textContent = post.email;
-
-    createTr.appendChild(createID);
-    createTr.appendChild(createName);
-    createTr.appendChild(createUsername);
-    createTr.appendChild(createEmail);
-    table.appendChild(createTr);
-  }
+  // Fuck this shit
 });
