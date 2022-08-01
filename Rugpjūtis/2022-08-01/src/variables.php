5ab26a9b06ede4b92da389b73b00186ef22b01d1
Edgaras <edgaras.rcns@gmail.com>
2022-08-01 19:40:52 +0300

    PHP

---
+++ Rugpjūtis/2022-08-01/src/variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = 'Edgaras';
+$surname = 'RČN';
+$number = 123456;
+$pi = 3.14;
+$hello = 'Hello world';
+
+define('PI', 3.14);
+
+var_dump(PI);
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(PHP_VERSION);
