01701c6861d7f45f049f9eae477c25ec370f7e44
Edgaras <edgaras.rcns@gmail.com>
2022-08-22 19:54:41 +0300

    update

--- Rugpjūtis/Docker/src/2022-08-02/index.php
+++ Rugpjūtis/Docker/src/2022-08-02/index.php
@@ -1,11 +1,11 @@
 <html>
     <head></head>
     <body>
-        <button><a href="http://localhost:8085/task1.php">Task 1</a></button>
-        <button><a href="http://localhost:8085/task2.php">Task 2</a></button>
-        <button><a href="http://localhost:8085/task3.php">Task 3</a></button>
-        <button><a href="http://localhost:8085/functions.php">Funkcijos</a></button>
-        <button><a href="http://localhost:8085/loops.php">Ciklai</a></button>
-        <button><a href="http://localhost:8085/alternative.php">ALT</a></button>
+        <button><a href="http://localhost/2022-08-02/task1.php">Task 1</a></button>
+        <button><a href="http://localhost/2022-08-02/task2.php">Task 2</a></button>
+        <button><a href="http://localhost/2022-08-02/task3.php">Task 3</a></button>
+        <button><a href="http://localhost/2022-08-02/functions.php">Funkcijos</a></button>
+        <button><a href="http://localhost/2022-08-02/loops.php">Ciklai</a></button>
+        <button><a href="http://localhost/2022-08-02/alternative.php">ALT</a></button>
     </body>
 </html>
