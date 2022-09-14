48eb0647b56946af65103d68c4f52f40fc122bdf
Edgaras <edgaras.rcns@gmail.com>
2022-09-14 20:30:58 +0300

    Connectors fix

--- Rugpjūtis/Docker/src/2022-09-14/Task/handload.php
+++ Rugpjūtis/Docker/src/2022-09-14/Task/handload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/Services/Connectors/FacebookConnector.php';
+require_once __DIR__ . '/Connectors/FacebookConnector.php';
 require_once __DIR__ . '/Services/Interfaces/Messenger.php';
 require_once __DIR__ . '/Services/Messengers/EmailMessengerService.php';
 require_once __DIR__ . '/Services/Messengers/FacebookMessengerService.php';
