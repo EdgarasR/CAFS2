4d6379b34fd537a81f89953c8330a5ad79df9919
Edgaras <edgaras.rcns@gmail.com>
2022-10-04 18:45:40 +0300

    MySQL Task 2

---
+++ Rugpjūtis/Docker/src/MySQL/2022-10-04/Task2/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/connect.php';
+
+if (isset($_GET['people'])) {
+
+    $query = 'SELECT * FROM `users` WHERE `birthdate` > "2004-01-01 00:00:00" ORDER BY `id` DESC LIMIT 13';
+
+    $stmt = $dbh->prepare($query);
+
+    $stmt->execute();
+
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($users);
+}
