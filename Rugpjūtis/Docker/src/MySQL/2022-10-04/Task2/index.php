5f82cc7edbbeddd04138f8cff970646e020c0f62
Edgaras <edgaras.rcns@gmail.com>
2022-10-04 20:03:51 +0300

    update

--- Rugpjūtis/Docker/src/MySQL/2022-10-04/Task2/index.php
+++ Rugpjūtis/Docker/src/MySQL/2022-10-04/Task2/index.php
@@ -2,15 +2,12 @@
 
 require_once __DIR__ . '/connect.php';
 
-if (isset($_GET['people'])) {
+$query = 'SELECT * FROM `users` WHERE `birthdate` > "2004-10-03 00:00:00" ORDER BY `id` DESC LIMIT 13';
 
-    $query = 'SELECT * FROM `users` WHERE `birthdate` > "2004-10-03 00:00:00" ORDER BY `id` DESC LIMIT 13';
+$stmt = $dbh->prepare($query);
 
-    $stmt = $dbh->prepare($query);
+$stmt->execute();
 
-    $stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    var_dump($users);
-}
+var_dump($users);
