987ad153253dab41e3e4825df96334388bd04983
Edgaras <edgaras.rcns@gmail.com>
2022-10-04 18:46:42 +0300

    MySQL Task 2

--- Rugpjūtis/Docker/src/MySQL/2022-10-04/Task2/index.php
+++ Rugpjūtis/Docker/src/MySQL/2022-10-04/Task2/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/connect.php';
 
 if (isset($_GET['people'])) {
 
-    $query = 'SELECT * FROM `users` WHERE `birthdate` > "2004-01-01 00:00:00" ORDER BY `id` DESC LIMIT 13';
+    $query = 'SELECT * FROM `users` WHERE `birthdate` > "2004-10-03 00:00:00" ORDER BY `id` DESC LIMIT 13';
 
     $stmt = $dbh->prepare($query);
 
